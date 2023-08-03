c9a09bfda2e5364db4b4253ab5844d7e658f465c
Sangam Gautam <sangam.gautam@griffithuni.edu.au>
2023-08-03 22:44:22 +1000

    Resolved merge conflict

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-﻿console.log("I caused a merge conflict");
+﻿console.log("I caused a merge conflict.");
+
+function add(a, b) {
+  return a + b;
+}
+
+console.log(`The sum of 5 and 10 is: ${add(5, 10)}`);
